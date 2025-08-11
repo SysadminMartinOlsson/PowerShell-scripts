b96a63cf3a1b0dba5f12d8cee7daed65d8aea97d
SysadminMartinOlsson <martin.olsson@conmodo.com>
2025-08-11 09:45:30 +0200

    Add files via upload

---
+++ Start-Windows11Upgrade.ps1
@@ -0,0 +1,214 @@
+<#PSScriptInfo
+
+.VERSION 2025.7.3
+
+.GUID 309659cf-0358-4996-9992-34f8a7dc09b9
+
+.AUTHOR Martin Olsson
+
+.COMPANYNAME Conmodo
+
+.COPYRIGHT (c) Conmodo. All rights reserved.
+
+.TAGS
+
+.LICENSEURI
+
+.PROJECTURI
+
+.ICONURI
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS
+
+.EXTERNALSCRIPTDEPENDENCIES
+
+.RELEASENOTES
+
+
+.PRIVATEDATA
+
+#>
+
+<# 
+
+.DESCRIPTION 
+ Upgrade a computer to Windows 11 
+
+#> 
+[CmdletBinding(SupportsShouldProcess)]
+param(
+    [switch]$Force
+)
+
+function Get-ScriptVersion {
+    param(
+        [Parameter(Mandatory)]
+        [ValidateNotNull()]
+        [string]$FilePath
+    )
+
+    if ([string](Get-Content -Path $FilePath) -match '\.VERSION (\d{4}\.\d{1,2}\.\d+)') {
+        $currentScriptVersion = $Matches[1]
+    }
+    if ([string]::IsNullOrEmpty($currentScriptVersion)) {
+        $currentScriptVersion = 'UNKNOWN'
+    }
+
+    return $currentScriptVersion
+}
+
+function Out-LogFile {
+    [CmdletBinding(SupportsShouldProcess)]
+    param(
+        [Parameter(Mandatory)]
+        [ValidateNotNull()]
+        [string]$FilePath,
+
+        [Parameter(Mandatory)]
+        [ValidateNotNull()]
+        [string]$ScriptVersion,
+
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [ValidateNotNull()]
+        [string]$Content
+    )
+
+    if (-not (Test-Path -Path (Split-Path -Path $FilePath -Parent))) {
+        Write-Verbose 'Creating log directory...'
+        New-Item -Path (Split-Path -Path $FilePath -Parent) -ItemType Directory
+    }
+
+    ('[{0} | v{1}] {2}' -f (Get-Date -Format 'yyyy-MM-dd HH:mm:ss'), $ScriptVersion, $Content) | Out-File -FilePath $FilePath -Append -ErrorAction Continue
+}
+
+$systemDrive = $env:SystemDrive
+if ([string]::IsNullOrEmpty($systemDrive)) {
+    $systemDrive = 'C:'
+}
+
+$scriptVersion = Get-ScriptVersion -FilePath (Join-Path -Path $PSScriptRoot -ChildPath $PSCmdlet.MyInvocation.MyCommand)
+$tempDirectoryPath = Join-Path -Path $systemDrive -ChildPath 'Temp'
+$logFilePath = Join-Path -Path $tempDirectoryPath -ChildPath 'Start-Windows11Upgrade.log'
+
+$logParams = @{
+    FilePath = $logFilePath
+    ScriptVersion = $scriptVersion
+}
+
+$installerDownloadUrl = 'https://go.microsoft.com/fwlink/?linkid=2171764'
+$installerFilePath = Join-Path -Path $tempDirectoryPath -ChildPath 'Windows11InstallationAssistant.exe'
+
+Write-Verbose "Logs will be stored in: $logFilePath"
+
+if (-not (Test-Path -Path $tempDirectoryPath)) {
+    Write-Verbose 'Creating temp directory...'
+    New-Item -Path $tempDirectoryPath -ItemType Directory
+}
+
+# Require run-as-admin.
+$WindowsIdentity = [Security.Principal.WindowsIdentity]::GetCurrent()
+$WindowsPrincipal = [Security.Principal.WindowsPrincipal] $WindowsIdentity
+$AdminRole = [Security.Principal.WindowsBuiltInRole]::Administrator
+if (-not $WindowsPrincipal.IsInRole($AdminRole)) {
+    Out-LogFile @logParams -Content 'The script needs to run as administrator.'
+    throw 'The script needs to run as administrator.'
+}
+
+# Determine if the system is valid for upgrade.
+$isSystemValidForUpgrade = $false
+$osName = (Get-ComputerInfo).OsName
+if ($osName -match 'Windows 10') {
+    $isSystemValidForUpgrade = $true
+}
+
+# Only upgrade if the system is valid for upgrade (or if using the Force parameter).
+if ($isSystemValidForUpgrade -or $Force) {
+    if ((Get-Command -Name 'Confirm-SecureBootUEFI' -ErrorAction SilentlyContinue | Measure-Object).Count -gt 0) {
+        try {
+            if (-not (Confirm-SecureBootUEFI -ErrorAction SilentlyContinue)) {
+                Write-Warning "Secure Boot is disabled. It's recommended (but not required) to enable it in the BIOS settings."
+                Out-LogFile @logParams -Content "Secure Boot is disabled. It's recommended (but not required) to enable it in the BIOS settings."
+            }
+        }
+        catch {
+            Write-Warning 'Unable to validate if Secure Boot is enabled. The upgrade can proceed anyway.'
+        }
+    }
+
+    $tpm = Get-WmiObject -Namespace 'Root\CIMv2\Security\MicrosoftTpm' -Class Win32_Tpm
+    if (($null -eq $tpm) -or (-not $tpm.IsEnabled_InitialValue) -or (-not $tpm.IsActivated_InitialValue)) {
+        Out-LogFile @logParams -Content 'TPM is not enabled (or not activated). Enable/activate the TPM in the BIOS settings to be able to upgrade.'
+        throw 'TPM is not enabled (or not activated). Enable/activate the TPM in the BIOS settings to be able to upgrade.'
+    }
+
+    # Allow upgrade on older devices.
+    $tpmMajorVersion = $tpm.SpecVersion.Split(',')[0] -as [int]
+    if ($tpmMajorVersion -lt 2) {
+        Write-Warning 'The script determined that this is an older device but will try to allow the upgrade anyway.'
+        try {
+            $registrySystemSetupPath = 'HKLM:\SYSTEM\Setup\MoSetup'
+            if (-not (Test-Path -Path $registrySystemSetupPath)) {
+                Write-Verbose 'Creating system setup registry key...'
+                New-Item -Path $registrySystemSetupPath -ErrorAction Stop
+            }
+            
+            if (Test-Path -Path $registrySystemSetupPath) {
+                $registryAllowUpgradeName = 'AllowUpgradesWithUnsupportedTPMOrCPU'
+                if ($null -eq (Get-ItemProperty -Path $registrySystemSetupPath -Name $registryAllowUpgradeName -ErrorAction SilentlyContinue)) {
+                    Write-Verbose 'Creating system setup registry value...'
+                    New-ItemProperty -Path $registrySystemSetupPath -Name $registryAllowUpgradeName -PropertyType 'DWORD' -Value 1 -ErrorAction Stop
+                    Out-LogFile @logParams -Content 'Added value to the registry to allow upgrade on an older device.'
+                }
+                elseif ((Get-ItemProperty -Path $registrySystemSetupPath -Name $registryAllowUpgradeName -ErrorAction SilentlyContinue).($registryAllowUpgradeName) -ne 1) {
+                    Write-Verbose 'Updating existing system setup registry value...'
+                    Set-ItemProperty -Path $registrySystemSetupPath -Name $registryAllowUpgradeName -Value 1 -ErrorAction Stop
+                    Out-LogFile @logParams -Content 'Updated value in the registry to allow upgrade on an older device.'
+                }
+            }
+            else {
+                Write-Error "Failed to find the registry key '$registrySystemSetupPath'."
+                Out-LogFile @logParams -Content "Failed to find the registry key '$registrySystemSetupPath'."
+            }
+        }
+        catch {
+            Write-Error "Failed to configure the registry to allow upgrade on an older device. $PSItem"
+            Out-LogFile @logParams -Content "Failed to configure the registry to allow upgrade on an older device. $PSItem"
+        }
+    }
+
+    # Upgrade the OS, then automatically restart the system.
+    if ($PSCmdlet.ShouldProcess("URL: $installerDownloadUrl", 'Download installer')) {
+        try {
+            $webClient = New-Object System.Net.WebClient
+            $webClient.DownloadFile($installerDownloadUrl, $installerFilePath)
+            Out-LogFile @logParams -Content "Downloaded the Windows 11 installation assistant to '$installerFilePath'."
+        }
+        catch {
+            Write-Error "Failed to download the Windows 11 installation assistant. $PSItem"
+            Out-LogFile @logParams -Content "Failed to downloaded the Windows 11 installation assistant. $PSItem"
+        }
+    }
+
+    if ($PSCmdlet.ShouldProcess("File: $installerFilePath", 'Start Windows 11 upgrade')) {
+        try {
+            Write-Verbose 'Started the Windows 11 upgrade.'
+            Out-LogFile @logParams -Content 'Started the Windows 11 upgrade.'
+
+            Start-Process -FilePath $installerFilePath -ArgumentList @('/QuietInstall /SkipEULA /Auto Upgrade /NoRestartUI /CopyLogs {0}' -f $tempDirectoryPath) -Wait
+            Write-Verbose 'Exited the upgrade process (the script cannot know if it was successful or not).'
+            Out-LogFile @logParams -Content 'Exited the upgrade process (the script cannot know if it was successful or not).'
+        }
+        catch {
+            Out-LogFile @logParams -Content "The Windows 11 upgrade failed. $PSItem"
+            throw "The Windows 11 upgrade failed. $PSItem"
+        }
+    }
+}
+else {
+    if (Test-Path -Path $installerFilePath -PathType Leaf) {
+        Remove-Item -Path $installerFilePath
+    }
+    throw "The script has determined that this isn't a Windows 10 system. Use the Force parameter if you want to bypass this check and run the upgrade anyway."
+}
