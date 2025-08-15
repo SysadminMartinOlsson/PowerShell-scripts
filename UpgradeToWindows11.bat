329d7a7b1c334770fbf80789cfbda147727694d8
SysadminMartinOlsson <martin.olsson@conmodo.com>
2025-08-15 15:11:45 +0200

    Update UpgradeToWindows11.bat

--- UpgradeToWindows11.bat
+++ UpgradeToWindows11.bat
@@ -1,2 +1,2 @@
 set SCRIPT="%~dp0Start-Windows11Upgrade.ps1"
-powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Start-Process powershell.exe -ArgumentList '-NoExit -ExecutionPolicy Bypass -File ""%SCRIPT%""' -Verb RunAs"
+powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Start-Process powershell.exe -ArgumentList '-NoExit -ExecutionPolicy Bypass -File ""%SCRIPT%"" -Verbose' -Verb RunAs"
